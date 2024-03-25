7b944bd895bea9027d8d86b1cf795a0dadbe853f
c2erichsu <c2erichsu@gmail.com>
2024-03-26 02:11:14 +0800

    x

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,4 @@
-/** @type {import('next').NextConfig} */
+
 import { join } from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
@@ -6,6 +6,7 @@ import { dirname } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
     reactStrictMode: true,
